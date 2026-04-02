e7cc94ad12de269bbc5adb5789c26d24c834191f
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2026-04-02 12:11:50 +0100

    build

---
+++ .wrangler/tmp/bundle-nZHRTq/middleware-insertion-facade.js
@@ -0,0 +1,10 @@
+				import worker, * as OTHER_EXPORTS from "/Users/JosephChikak/Developer/clientWork/digital-archive/node_modules/@opennextjs/cloudflare/dist/cli/workers/r2-cache.js";
+				import * as __MIDDLEWARE_0__ from "/Users/JosephChikak/Developer/clientWork/digital-archive/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+
+				export * from "/Users/JosephChikak/Developer/clientWork/digital-archive/node_modules/@opennextjs/cloudflare/dist/cli/workers/r2-cache.js";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default
+				]
+				export default worker;
